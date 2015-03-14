2b3611353be48617e90421ddee8950c2907881a2
Kostis Karantias <karantiaskostis@gmail.com>
2015-03-14 15:50:02 +0200

    Implement hex2i

--- lab03/hex2i.asm
+++ lab03/hex2i.asm
@@ -20,10 +20,52 @@ main:
         # Write your program here
         # The following assumes the result is in $s0
         #################################
+        
+        # Find the least significant digit of the number and start
+        # from there in order to support variable-length strings.
+        
+        li   $s0, 0
+        li   $t3, 0
+        add  $a3, $a0, $zero # $a3 stores the start of the string
+        add  $a2, $a0, $zero
+        
+findEnd:
+        lbu  $t0, 0($a2)
+        beq  $t0, $zero, foundEnd
+        addu $a2, $a2, 1
+        j findEnd
+        
+foundEnd:
+        addu $a2, $a2, -1
+        
+loop:      
+        lbu  $t0, 0($a2)
+        
+        slti $t2, $t0, 58
+        beq  $t2, $zero, letter
+        # subtract from '0'
+        addi $t0, $t0, -48
+        j continue
 
+letter:
+        # subtract from 'A', add 10
+        addi $t0, $t0, -55
+	
+continue:
+        sllv $t0, $t0, $t3
+        addu $s0, $s0, $t0
+        
+        beq  $a2, $a3, save
+        
+        addiu $a2, $a2, -1
+        addiu $t3, $t3, 4
+        j loop
+    
+        #################################
+save:
         la   $a1, number # Address where the result should go to
         sw   $s0, 0($a1)
-
+exit:
         # end the program
         li   $v0, 10
         syscall
