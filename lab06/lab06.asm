1e6e7ae96c4f225b1d1df58b6fd029cee3d94e3d
Kostis Karantias <karantiaskostis@gmail.com>
2015-04-26 15:53:19 +0300

    Completed step 1

--- lab06/lab06.asm
+++ lab06/lab06.asm
@@ -7,5 +7,17 @@ number:
 
         .text
 main:   
-	add $t1, $zero, $zero
+	lw  $t1, 0($zero)
+	add $t2, $t1, $t1
+	sub $t3, $t2, $t1
+	and $t4, $t3, $zero
+	or  $t5, $t3, $zero
 	
+	beq $t5, $t5, beqValid
+beqValid:
+	beq $t5, $zero, beqInvalid
+	j end
+beqInvalid:
+	j main
+end:
+	add $t6, $zero, $zero
