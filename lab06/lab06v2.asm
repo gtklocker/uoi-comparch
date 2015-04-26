3322b3e6f48a324e55b0b3f660397d48be431193
Kostis Karantias <karantiaskostis@gmail.com>
2015-04-26 15:54:42 +0300

    Completed steps 2, 3

---
+++ lab06/lab06v2.asm
@@ -0,0 +1,25 @@
+
+        .data
+number:
+        .word  15
+       
+        .globl main
+
+        .text
+main:   
+	lw  $t1, 0($zero)
+	add $t2, $t1, $t1
+	sub $t3, $t2, $t1
+	and $t4, $t3, $zero
+	or  $t5, $t3, $zero
+	
+	beq $t5, $t5, beqValid
+beqValid:
+	beq $t5, $zero, beqInvalid
+	j end
+beqInvalid:
+	j main
+end:
+	add $t6, $zero, $zero
+	andi $t7, $t1, 0
+	andi $t8, $t1, 0xff
